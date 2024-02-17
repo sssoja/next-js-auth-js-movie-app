3ac672dc96ca5521f1a2d4c21c7c19609ffa8b12
Sophie <sophie@sssoja.com>
2024-02-17 17:40:48 +0000

    fix: handle no favourites found

--- components/movies/FavouriteMovies.tsx
+++ components/movies/FavouriteMovies.tsx
@@ -8,6 +8,8 @@ import { fetchMovie } from "@/hooks/useMovieById";
 const FavouriteMovies = (page: any) => {
   const items = localStorage.getItem("favourites")?.split(",");
 
+  const noFavourites = !localStorage.getItem("favourites");
+
   const outputArray = items?.map(item =>
     parseInt(item.replace(/\[|\]/g, ""), 10)
   );
@@ -16,24 +18,33 @@ const FavouriteMovies = (page: any) => {
     useQuery(["movie", id], () => fetchMovie(id))
   );
 
-  const movies = queries?.map((movie: UseQueryResult<IMovie>) => movie.data);
+  const movies = queries?.map((query: UseQueryResult<IMovie>) => query.data);
+
+  if (noFavourites) {
+    return (
+      <p className="text-sm font-medium">
+        Start adding your favourite movies...
+      </p>
+    );
+  }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between items-center mt-4">
-        <h1 className="text-2xl font-medium">
-          Favourite Movies{" "}
-          {outputArray && outputArray.length > 0 && `(${outputArray.length})`}
-        </h1>
+    movies && (
+      <div className="flex flex-col">
+        <div className="flex justify-between items-center my-3">
+          <h1 className="text-2xl font-medium">
+            Favourite Movies {movies.length > 0 && `(${movies.length})`}
+          </h1>
+        </div>
+        {movies.length > 0 && (
+          <div className="grid grid-cols-1 sm:grid-cols-4 mt-4 gap-4">
+            {movies.map((movie: IMovie | undefined) => (
+              <MovieCard key={movie?.id} movie={movie} />
+            ))}
+          </div>
+        )}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-4 mt-4 gap-4">
-        {movies &&
-          movies.length > 0 &&
-          movies.map((movie: any) => (
-            <MovieCard key={movie?.id} movie={movie} />
-          ))}
-      </div>
-    </div>
+    )
   );
 };
 
--- components/movies/MovieCard.tsx
+++ components/movies/MovieCard.tsx
@@ -7,37 +7,39 @@ import { IMovie } from "./MovieDetails";
 import FavouriteButton from "../FavouriteButton";
 import { useSession } from "next-auth/react";
 
-const MovieCard = ({ movie }: { movie: IMovie }) => {
+const MovieCard = ({ movie }: { movie: IMovie | undefined }) => {
   const { data: session, status } = useSession();
 
   return (
-    <div className="flex flex-col">
-      <Link href={`/movie/${movie?.id}`}>
-        <div className="w-full h-[400px] relative ease-in duration-300 hover:scale-105">
-          <Image
-            src={
-              movie?.poster_path
-                ? `${tmdb_api_image_url}${movie?.poster_path}`
-                : `${tmdb_api_placeholder_image}`
-            }
-            alt={movie?.title}
-            fill={true}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-      </Link>
-      <div className="flex gap-4 justify-between mt-3">
-        <h2 className="text-lg font-medium">{movie?.title}</h2>
-        <div className="flex items-start">
-          {session && status === "authenticated" && (
-            <FavouriteButton id={movie?.id} />
-          )}
-          <span className="p-2 ml-2 rounded-md bg-indigo-700">
-            {roundToDecimal(movie?.vote_average, 1)}
-          </span>
+    movie && (
+      <div className="flex flex-col">
+        <Link href={`/movie/${movie.id}`}>
+          <div className="w-full h-[400px] relative ease-in duration-300 hover:scale-105">
+            <Image
+              src={
+                movie.poster_path
+                  ? `${tmdb_api_image_url}${movie.poster_path}`
+                  : `${tmdb_api_placeholder_image}`
+              }
+              alt={movie.title}
+              fill={true}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+        </Link>
+        <div className="flex gap-4 justify-between mt-3">
+          <h2 className="text-lg font-medium">{movie.title}</h2>
+          <div className="flex items-start">
+            {session && status === "authenticated" && (
+              <FavouriteButton id={movie.id} />
+            )}
+            <span className="p-2 ml-2 rounded-md bg-indigo-700">
+              {roundToDecimal(movie.vote_average, 1)}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
