6c913b567e9e49d7f133cba8d8288d6ea4ba038a
Sophie <sophie@sssoja.com>
2024-02-17 20:30:19 +0000

    fix: protect favourites route when not signed in

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,9 +1,23 @@
 import FavouriteMovies from "@/components/movies/FavouriteMovies";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-16 py-8 sm:px-20">
-      <FavouriteMovies />
+      {session ? (
+        <FavouriteMovies />
+      ) : (
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center mt-4">
+            <h1 className="text-2xl font-medium">
+              Sign in to view your favourite movies
+            </h1>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
